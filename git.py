eba2c4dde915ef29c8a738d85df5232094f0f7d4
Sander Vanhove <sandervhove@gmail.com>
2021-05-09 23:15:51 +0100

    Add date to values.json

--- git.py
+++ git.py
@@ -4,6 +4,7 @@ from os.path import isdir
 import json
 from dataclasses import asdict
 from shutil import copyfile
+from datetime import datetime
 
 from sensors import SensorReading
 from config import Config
@@ -20,7 +21,9 @@ def commit_data(metric_values: SensorReading, pic_path: str, config: Config):
     # Add the current values and picture
     values_path = path.join(GIT_DIR, "values.json")
     with open(values_path, "w") as values_file:
-        json.dump(asdict(metric_values), values_file)
+        values_dict = asdict(metric_values)
+        values_dict["date"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        json.dump(values_dict, values_file)
 
     git_pic_path = path.join(GIT_DIR, "pic.jpg")
     copyfile(pic_path, git_pic_path)
